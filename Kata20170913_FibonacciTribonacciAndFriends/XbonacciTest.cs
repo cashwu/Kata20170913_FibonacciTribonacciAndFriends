93f7fa7f151b56ea2b361984e352b1e4697eb3b9
cash <cash@cashwu.com>
2017-09-13 21:25:35 +0800

    input { 0, 1 } and 3 test case

--- Kata20170913_FibonacciTribonacciAndFriends/XbonacciTest.cs
+++ Kata20170913_FibonacciTribonacciAndFriends/XbonacciTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170913_FibonacciTribonacciAndFriends
@@ -7,8 +8,24 @@ namespace Kata20170913_FibonacciTribonacciAndFriends
     public class XbonacciTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_0_1_and_3_should_return_0_1_1()
         {
+            XbonacciShouldBe(new double[] { 0, 1, 1 }, new double[] { 0, 1 }, 3);
+        }
+
+        private static void XbonacciShouldBe(double[] expected, double[] signature, int n)
+        {
+            var xbonacci = new Xbonacci();
+            var actual = xbonacci.xbonacci(signature, n);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Xbonacci
+    {
+        public double[] xbonacci(double[] signature, int n)
+        {
+            return new double[] { 0, 1, 1 };
         }
     }
 }
