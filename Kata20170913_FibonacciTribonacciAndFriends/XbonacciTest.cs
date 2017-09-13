16468c10363f15a3900b833084a67bddb0f106eb
cash <cash@cashwu.com>
2017-09-13 21:26:40 +0800

    input {1,1} and 3 test case

--- Kata20170913_FibonacciTribonacciAndFriends/XbonacciTest.cs
+++ Kata20170913_FibonacciTribonacciAndFriends/XbonacciTest.cs
@@ -13,6 +13,12 @@ namespace Kata20170913_FibonacciTribonacciAndFriends
             XbonacciShouldBe(new double[] { 0, 1, 1 }, new double[] { 0, 1 }, 3);
         }
 
+        [TestMethod]
+        public void input_1_1_and_3_should_return_1_1_2()
+        {
+            XbonacciShouldBe(new double[] { 1, 1, 2 }, new double[] { 1, 1 }, 3);
+        }
+
         private static void XbonacciShouldBe(double[] expected, double[] signature, int n)
         {
             var xbonacci = new Xbonacci();
@@ -25,7 +31,7 @@ namespace Kata20170913_FibonacciTribonacciAndFriends
     {
         public double[] xbonacci(double[] signature, int n)
         {
-            return new double[] { 0, 1, 1 };
+            return new[] { signature[0], signature[1], signature[0] + signature[1] };
         }
     }
 }
