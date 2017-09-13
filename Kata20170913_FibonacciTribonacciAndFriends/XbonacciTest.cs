3f9b973d715952f8516750fd3b0181c7e556689e
cash <cash@cashwu.com>
2017-09-13 22:09:52 +0800

    input { 0,1,1,1} and 6 test case

--- Kata20170913_FibonacciTribonacciAndFriends/XbonacciTest.cs
+++ Kata20170913_FibonacciTribonacciAndFriends/XbonacciTest.cs
@@ -27,6 +27,24 @@ namespace Kata20170913_FibonacciTribonacciAndFriends
             XbonacciShouldBe(new double[] { 1, 1, 2, 3 }, new double[] { 1, 1 }, 4);
         }
 
+        [TestMethod]
+        public void input_0_1_and_10()
+        {
+            XbonacciShouldBe(new double[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 }, new double[] { 0, 1 }, 10);
+        }
+
+        [TestMethod]
+        public void input_1_1_and_10()
+        {
+            XbonacciShouldBe(new double[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 }, new double[] { 1, 1 }, 10);
+        }
+
+        [TestMethod]
+        public void input_0_1_1_1_and_6_should_return_0_1_1_1_3_6()
+        {
+            XbonacciShouldBe(new double[] { 0, 1, 1, 1, 3, 6 }, new double[] { 0, 1, 1, 1 }, 6);
+        }
+
         private static void XbonacciShouldBe(double[] expected, double[] signature, int n)
         {
             var xbonacci = new Xbonacci();
@@ -40,12 +58,21 @@ namespace Kata20170913_FibonacciTribonacciAndFriends
         public double[] xbonacci(double[] signature, int n)
         {
             var result = signature.ToList();
-            for (int i = 0; i < n; i++)
+            for (var i = 0; i < n; i++)
             {
-                if (i + 1 > result.Count)
+                if (i + 1 <= result.Count)
                 {
-                    result.Add(result[i - 1] + result[i - 2]);
+                    continue;
                 }
+
+                var val = result[i - 1] + result[i - 2]; 
+
+                if (signature.Length > 2)
+                {
+                    val += result[i - 3] + result[i - 4];
+                }
+
+                result.Add(val);
             }
 
             return result.ToArray();
