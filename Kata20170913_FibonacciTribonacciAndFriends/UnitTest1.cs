1e184d63d8a313c020dc559335ee6e446ac01485
cash <cash@cashwu.com>
2017-09-13 21:20:05 +0800

    Add project files.

---
+++ Kata20170913_FibonacciTribonacciAndFriends/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170913_FibonacciTribonacciAndFriends
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
